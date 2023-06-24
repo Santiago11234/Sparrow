ea67ba92dd98418e2fefef7405d03db82ae6173d
eva li <lievayifan@gmail.com>
2023-06-23 22:06:43 -0500

    drag and drop, anim

--- newdrag.js
+++ newdrag.js
@@ -1,11 +1,13 @@
 // Initialize all the components that were made in HTML
-let lists = document.getElementsByClassName("list");
+let lists = document.getElementsByClassName("listDragDrop");
 let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
 let final = [];
 let button = document.getElementById("check");
 let correct = true;
 let cloneCount = 1; // initialize the clone counter
+let player = document.getElementById("player");
+let grid = document.getElementById("playground");
 
 // add event listener to each block to enable dragging
 // when a block is dragged, its ID will be the data transferred
@@ -106,17 +108,146 @@ rightBox.addEventListener("drop", function(e) {
       
 });
 
+
+//ignore this for the playground
+// button.addEventListener("click", function() {
+//   for(let i = 0; i < final.length; i++) {
+//     console.log(final[i].id.substring(0, 1));
+//     if(final[i].id.substring(0, 1) != (i + 1) ) {
+//       correct = false;
+//     }
+//   }
+  
+//   if (correct) {
+//     alert("Correct");
+//   } else {
+//     alert("Incorrect");
+//   }
+// });
+
+//running code section
+// Define the initial direction
+let direction = 'up';
+
 button.addEventListener("click", function() {
-  for(let i = 0; i < final.length; i++) {
-    console.log(final[i].id.substring(0, 1));
-    if(final[i].id.substring(0, 1) != (i + 1) ) {
-      correct = false;
+  let commands = [];
+  
+  // Get the current player position
+  let currentPosition = getPlayerPosition();
+  let currentColumn = currentPosition.gridColumn;
+  let currentRow = currentPosition.gridRow;
+
+  for (let i = 0; i < final.length; i++) {
+    let blockId = "" + final[i].id.substring(0, 1);
+    switch (blockId) {
+      case '1':
+        // Move forward by 1 grid based on the current direction
+        if (direction === 'up') {
+          commands.push('forward');
+        } else if (direction === 'right') {
+          commands.push('right');
+        } else if (direction === 'down') {
+          commands.push('back');
+        } else if (direction === 'left') {
+          commands.push('left');
+        }
+        break;
+      case '2':
+        // Turn right
+        commands.push('right');
+        break;
+      case '3':
+        // Turn left
+        commands.push('left');
+        break;
+      case '4':
+        // Move back by 1 grid based on the current direction
+        if (direction === 'up') {
+          commands.push('back');
+        } else if (direction === 'right') {
+          commands.push('left');
+        } else if (direction === 'down') {
+          commands.push('forward');
+        } else if (direction === 'left') {
+          commands.push('right');
+        }
+        break;
+      default:
+        console.log("Invalid command");
+        break;
     }
   }
-  
-  if (correct) {
-    alert("Correct");
-  } else {
-    alert("Incorrect");
-  }
+  executeCommands(commands);
 });
+
+function movePlayer(column, row) {
+  player.style.gridColumnStart = column;
+  player.style.gridRowStart = row;
+}
+
+function getPlayerPosition() {
+  let gridColumn = parseInt(getComputedStyle(player).gridColumnStart);
+  let gridRow = parseInt(getComputedStyle(player).gridRowStart);
+  return { gridColumn, gridRow };
+}
+
+function executeCommands(commands) {
+  let delay = 500; // Adjust the delay as needed (in milliseconds)
+
+  for (let i = 0; i < commands.length; i++) {
+    setTimeout(function () {
+      let command = commands[i];
+      let currentPosition = getPlayerPosition();
+
+      if (command === 'forward') {
+        movePlayer(currentPosition.gridColumn+1, currentPosition.gridRow);
+      } else if (command === 'right') {
+        direction = turnRight(direction);
+        movePlayer(currentPosition.gridColumn, currentPosition.gridRow);
+      } else if (command === 'left') {
+        direction = turnLeft(direction);
+        movePlayer(currentPosition.gridColumn, currentPosition.gridRow);
+      } else if (command === 'back') {
+        movePlayer(currentPosition.gridColumn-1, currentPosition.gridRow);
+      }
+    }, delay * i);
+  }
+}
+
+function turnRight(direction) {
+  if (direction === 'up') {
+    turnPlayer('right');
+    return 'right';
+  } else if (direction === 'right') {
+    turnPlayer('down');
+    return 'down';
+  } else if (direction === 'down') {
+    turnPlayer('left');
+    return 'left';
+  } else if (direction === 'left') {
+    turnPlayer('up');
+    return 'up';
+  }
+}
+
+function turnLeft(direction) {
+  if (direction === 'up') {
+    turnPlayer('left');
+    return 'left';
+  } else if (direction === 'right') {
+    turnPlayer('up');
+    return 'up';
+  } else if (direction === 'down') {
+    turnPlayer('right');
+    return 'right';
+  } else if (direction === 'left') {
+    turnPlayer('down');
+    return 'down';
+  }
+}
+
+function turnPlayer(direction) {
+  player.style.transform = 'rotate(90deg)';
+  if (direction === 'right') {
+    direction
+}
